9e55ae859308d0e78666b8e2d5474a7ef36470e1
Nicholas Samuel Granlund <102576701+NicholasGranlund@users.noreply.github.com>
2023-09-05 16:53:26 +0200

    Update InitializePopulation.m

--- HP1/SkeletonFiles_HP1.3/InitializePopulation.m
+++ HP1/SkeletonFiles_HP1.3/InitializePopulation.m
@@ -1,1 +1,10 @@
 function population = InitializePopulation(populationSize,numberOfGenes);
+    % Name: Nicholas Granlund
+    % Date: 2022-04-14
+    % Description: This function instantiates a population given the population size and
+    % the number of parameters. The function returns a matrix with 
+    % rows=individuals and columns=genes. 
+    
+    population = rand(populationSize, numberOfGenes);
+
+end
